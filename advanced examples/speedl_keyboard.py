06a8518329940cb749d3d6c45bc8d26d26834bb1
Luke <work37@gmail.com>
2018-10-22 17:36:14 +0300

    speedL Rotation added

--- advanced examples/speedl_keyboard.py
+++ advanced examples/speedl_keyboard.py
@@ -17,23 +17,43 @@ while running:
 
     vel = 0.1
     acc = 0.5
+    rad_vel = 1
     t = 1
 
     x = 0
     y = 0
     z = 0
+    rx = 0
+    ry = 0
+    rz = 0
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        y = vel
-    if keys[pygame.K_DOWN]:
-        y = -vel
     if keys[pygame.K_LEFT]:
         x = -vel
     if keys[pygame.K_RIGHT]:
         x = vel
+    if keys[pygame.K_UP]:
+        y = vel
+    if keys[pygame.K_DOWN]:
+        y = -vel
+    if keys[pygame.K_PAGEUP]:
+        z = vel
+    if keys[pygame.K_PAGEDOWN]:
+        z = -vel
+    if keys[pygame.K_q]:
+        rx = -rad_vel
+    if keys[pygame.K_w]:
+        rx = rad_vel
+    if keys[pygame.K_a]:
+        ry = -rad_vel
+    if keys[pygame.K_s]:
+        ry = rad_vel
+    if keys[pygame.K_z]:
+        rz = -rad_vel
+    if keys[pygame.K_x]:
+        rz = rad_vel
 
-    cmd = "speedl([{x},{y},{z}, 0, 0, 0], {acc}, {time})\n".format(x=x, y=y, z=z, acc=acc, time=t)
+    cmd = "speedl([{x},{y},{z}, {rx}, {ry}, {rz}], {acc}, {time})\n".format(x=x, y=y, z=z, rx=rx, ry=ry, rz=rz, acc=acc, time=t)
     s.send((cmd).encode())
     data = s.recv(1024)
 
