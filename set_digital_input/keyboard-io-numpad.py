96d77d352c63cb0fbbe63d4c2a4ad4ec9885a147
Eugene Israelit <ei@native-robotics.com>
2021-12-16 04:40:59 +0300

    minor code cleanup

--- set_digital_input/keyboard-io-numpad.py
+++ set_digital_input/keyboard-io-numpad.py
@@ -6,7 +6,7 @@ import pygame
 def set_digital_in(socket, port, value):
     cmd = "sec di():\nset_digital_in({port}, {val})\nend\n".format(port=port, val=value)
     socket.send(cmd.encode())
-    data = socket.recv(1024)
+    socket.recv(1024)
 
 
 def set_digital_inputs(states):
@@ -19,11 +19,11 @@ def draw_states(states):
         color = (0, 0, 0)
         if state:
             color = (0, 200, 0)
-        pygame.draw.circle(screen, color, (size, i*size*2+size), size*0.9)
+        pygame.draw.circle(screen, color, (size, i * size * 2 + size), size * 0.9)
 
-        font = pygame.font.SysFont('arial', int(size*1.5))
+        font = pygame.font.SysFont('arial', int(size * 1.5))
         text = font.render(str(i), True, (255, 255, 255))
-        screen.blit(text, (int(size*0.6), i*size*2+int(size*0.3)))
+        screen.blit(text, (int(size * 0.6), i * size * 2 + int(size * 0.3)))
 
 
 keys = [pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4,
@@ -32,7 +32,6 @@ states = []
 for key in keys:
     states.append(False)
 
-
 HOST = "127.0.0.1"  # The remote host
 PORT = 30003  # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,21 +39,21 @@ s.connect((HOST, PORT))
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
 pygame.init()
-size=12
-width=size*2
-height=size*2*len(states)
+size = 12
+width = size * 2
+height = size * 2 * len(states)
 screen = pygame.display.set_mode((width, height))
 
 running = True
 while running:
 
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
 
-        if i.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             for n, key in enumerate(keys):
-                if i.key == key:
+                if event.key == key:
                     states[n] = not states[n]
 
     set_digital_inputs(states)
